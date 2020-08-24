5e385b4fd06160b8d3cbf9c7b768d16b2de3fb10
Aman Chaudhry <amanchaudhry@Amans-MacBook-Pro.local>
2020-08-24 14:32:46 -0600

    Adding questions

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ const inquirer=require("inquirer")
 inquirer.prompt([
     {
         type:"input",
-        name:"color",
-        message:"what is your favorite color?"
+        name:"title",
+        message:"what is the title?"
     },
     {
         type:"input",
-        name:"fruit",
-        message:"what is your favorite fruit?"
-    }
+        name:"description",
+        message:"what is the description?"
+    },
+    {
+        type:"input",
+        name:"table",
+        message:"what is the table of contents?"
+    },
+    {
+        type:"input",
+        name:"installation",
+        message:"what is the installation?"
+    },
+    {
+        type:"input",
+        name:"usage",
+        message:"what is the usage?"
+    },
 ]).then(function(answers){
     console.log(answers)
 })
