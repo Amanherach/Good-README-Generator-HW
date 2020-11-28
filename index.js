532309905f6dece3440953eddbeff8d6b2d3534a
Aman Chaudhry <aman.chaudhry@hotmail.com>
2020-11-28 00:26:25 -0700

    index.js

--- index.js
+++ index.js
@@ -1,62 +1,94 @@
-const fs = require("fs")
-const inquirer = require("inquirer")
-const generateMarkdown= require("./generateMarkdown")
+//create fixed variables
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require("axios");
+const generate = require('./utils/generateMarkdown');
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you want:"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description:"
+    },
+    {
+        type: "type",
+        name: "installation",
+        message: "Please provide the installation instructions:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage:"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please provide the project license or your badge link:"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the project contributing parties:"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide project tests:"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
 
 ];
+//use of inquirer node for asking commands or prompts
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const queryUrl = `https://api.github.com/users/${data.username}`;
+        //use of axios node to retrieve information from an api or DOM
+        axios.get(queryUrl).then(function(res) {
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+          // function to write README file
+          fs.writeFile("README.md", generate(data, githubInfo), function(err){
+            if (err) {
+                throw err;
+            };
 
-// function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
-}
+            console.log("New README file created with success!");
+          });
+
+        });
+});
+          // function to write README file option 2
+          // function writeToFile(fileName, data) {
+          // }
 
 // function to initialize program
 function init() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "what is the title?"
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "what is the description?"
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "what is the installation?"
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "what is the usage?"
-        },
-        {
-            type: "input",
-            name: "contributing",
-            message: "how does one contribute?"
-        },
-        {
-            type: "input",
-            name: "tests",
-            message: "how do you test your application?"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "what is license does your project use?",
-            choices: ["MIT","GPLv2"]
-        },
-    ]).then(function (answers) {
-        const mdText= generateMarkdown(answers)
-        writeToFile("output.md",mdText)
-    })
+
 }
 
 // function call to initialize program
